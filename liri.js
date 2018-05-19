f0b513f167c0f00f146fe40df3f8e76ca4cd5a66
balbano80 <balbano80@gmail.com>
2018-05-19 16:53:48 -0700

    added inquirer questions if no arguments are passed from command line.  If an argument(s) are passed, goes into switch case block

--- liri.js
+++ liri.js
@@ -6,55 +6,112 @@ var inquirer = require("inquirer");
 var request= require("request");
 var keys = require("./keys.js");
 var fs = require("fs");
-//try nested inquirer calls for userinput
-    //first one to determine the call the user wants to make(twitter, spotify, omdb, do-this)
-        //if spotify or omdb
-            // try another inquirer prompt inside the .then of the original prompt
+var choice = "Exit";
+
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
 var command = process.argv[2];
 var userInput = "";
 
-if (process.argv[3] !== undefined){
-    for (var i = 3; i < process.argv.length; i++){
-        userInput += process.argv[i] + " ";
-    }
-};
-// console.log("UserInput: " + userInput);
-switch(command){
-    case "my-tweets":
-        getTweets(userInput);
-        break;
-
-    case "spotify-this-song":
-        getSpotify(userInput);
-        break;
-
-    case "movie-this":
-        if (userInput === ""){
-            userInput = "Mr. Nobody"
+if (command === undefined){
+inquirer
+    .prompt([
+        { type: "list",
+          name: "choice",
+          message: "Which option would you like to try?",
+          choices: ["Check tweets on a subject", "Get Spotify information on a song title", 
+          "Check OMDB based on a movie title", "Surprise Me"],
         }
-        getMovie(userInput);
-        break;
-
-    case "do-what-it-says":
-        doThis()
-        break;
+    ]).then(function(userResponse){
+        console.log(userResponse);
+        choice = userResponse.choice;
+        if (userResponse.choice === "Check tweets on a subject"){
+            inquirer
+                .prompt([
+                    { type: "input",
+                      name: "tweetTopic",
+                      message: "Please type in the topic of tweets your would like to search for: ",                   
+                    }
+                ]).then(function(userTopic){
+                    getTweets(userTopic.tweetTopic);
+                })
+        }
+        else if (userResponse.choice === "Get Spotify information on a song title"){
+            inquirer
+                .prompt([
+                    { type: "input",
+                      name: "songTitle",
+                      message: "Which song title would you like to search through Spotify?",                   
+                    }
+                ]).then(function(userTopic){
+                    getSpotify(userTopic.songTitle);
+                })
+        }
+        else if (userResponse.choice === "Check OMDB based on a movie title"){
+            inquirer
+                .prompt([
+                    { type: "input",
+                      name: "movieTitle",
+                      message: "Which movie title would you like to search through OMDB?",                   
+                    }
+                ]).then(function(userTopic){
+                    getMovie(userTopic.movieTitle);
+                })
+        }
+        else{
+            doThis();
+        }
+    })
 }
 
+else{
+    if (process.argv[3] !== undefined){
+        for (var i = 3; i < process.argv.length; i++){
+            userInput += process.argv[i] + " ";
+        }
+    };
+    // console.log("UserInput: " + userInput);
+    switch(command){
+        case "my-tweets":
+            getTweets(userInput);
+            break;
+
+        case "spotify-this-song":
+            if (userInput === ""){
+                userInput = "The Sign";
+                console.log(userInput);
+            }
+            getSpotify(userInput);
+            break;
+
+        case "movie-this":
+            if (userInput === ""){
+                userInput = "Mr. Nobody"
+            }
+            getMovie(userInput);
+            break;
+
+        case "do-what-it-says":
+            doThis()
+            break;
+    }
+}
 function getTweets(topic){
     client.get("search/tweets", 
     {
         q: topic,
         result_type: "recent"
     }, function(error, tweets, responses){
+        if (error){
+            console.log(error);
+        }
+        dataLog(JSON.stringify(tweets));
         console.log("+ + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + +");
         console.log("        ### Tweets ###");
         console.log(tweets);
         console.log("= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =");
         console.log("");
-        dataLog(JSON.stringify(tweets));
         // console.log(responses);
     });
 } //reach out to twitter and grab tweets related to userInput topic
@@ -68,6 +125,7 @@ function getSpotify(song){
         if (err){
             return console.log("Error ocurred " + err);
         }
+        dataLog(JSON.stringify(data.tracks.items[0]));
         console.log("+ + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + +");
         console.log("Artist: " + data.tracks.items[0].artists[0].name);
         console.log("Track name: " + data.tracks.items[0].name);
@@ -75,7 +133,6 @@ function getSpotify(song){
         console.log("Album name: " + data.tracks.items[0].album.name);
         console.log("= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =");
         console.log("");
-        dataLog(JSON.stringify(data.tracks.items[0]));
     });
 
 }//reach out to spotify and grab userInput song information
@@ -83,6 +140,8 @@ function getSpotify(song){
 function getMovie(movie){
     request("http://www.omdbapi.com/?t=" + movie+ "&y=&plot=short&apikey=trilogy", function(error, response, body) {
         if (!error && response.statusCode === 200) {
+            dataLog(JSON.stringify(body));
+            // console.log(body);
             console.log("+ + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + +");
             console.log("Movie title: " + JSON.parse(body).Title);
             console.log("Year released: " + JSON.parse(body).Year);
@@ -94,7 +153,6 @@ function getMovie(movie){
             console.log("Actors: " + JSON.parse(body).Actors);
             console.log("= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =");
             console.log("");
-            dataLog(JSON.stringify(body));
         }
         else{
             console.log("Error Status Code: " + response.statusCode)
@@ -109,9 +167,9 @@ function doThis(){
             console.log(error);
         }
         data = data.replace(/\n|\r/g, "");
-        console.log(data);
+        // console.log(data);
         var dataArr = data.split(",");
-        console.log(dataArr);
+        // console.log(dataArr);
         for (var i = 0; i < dataArr.length; i+=2){
             switch(dataArr[i]){
                 case "spotify-this-song":
